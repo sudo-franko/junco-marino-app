6dafa251ef05fd298cfed3d245ab2217201a1ad9
Renzo Anacleto <ranacleto@ucvvirtual.edu.pe>
2024-10-15 15:21:03 -0500

    Conexion de prueba con API

--- app/src/main/java/com/example/juncomarinoapp/MainActivity.java
+++ app/src/main/java/com/example/juncomarinoapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.juncomarinoapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvMenu;
+    private ArrayList<String> menuItems;
+    private ArrayAdapter<String> adapter;
+    private RequestQueue rq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lvMenu = findViewById(R.id.lvMenu);
+        menuItems = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuItems);
+        lvMenu.setAdapter(adapter);
+
+        String url = "http://192.168.1.37:4000/listarPlatillos";
+        rq = Volley.newRequestQueue(this);
+
+        JsonObjectRequest requerimiento = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray platillos = response.getJSONArray("productos");
+
+                            for (int i = 0; i < platillos.length(); i++) {
+                                JSONObject plato = platillos.getJSONObject(i);
+                                String nombre = plato.getString("nombre");
+                                String descripcion = plato.getString("descripcion");
+                                String precio = plato.getString("precio");
+
+                                menuItems.add(nombre + " \n" + descripcion + " \nS/." + precio);
+                            }
+
+                            adapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        rq.add(requerimiento);
+
     }
 }
