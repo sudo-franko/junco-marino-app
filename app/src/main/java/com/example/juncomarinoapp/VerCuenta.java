9a978590fad92a4d6a7d4451cc68005204b01d97
Renzo Anacleto <ranacleto@ucvvirtual.edu.pe>
2024-11-05 12:47:56 -0500

    Finalizado avance 2

--- app/src/main/java/com/example/juncomarinoapp/VerCuenta.java
+++ app/src/main/java/com/example/juncomarinoapp/VerCuenta.java
@@ -9,8 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.juncomarinoapp.modelo.dto.Usuario;
 
 public class VerCuenta extends Fragment {
+
+    private TextView tvNombres, tvCorreo, tvTelefono, tvDireccion;
 
     public VerCuenta() {
         // Required empty public constructor
@@ -26,5 +31,20 @@ public class VerCuenta extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        enlazarControles(view);
+        if (getActivity() instanceof MainActivity2) {
+            Usuario u = ((MainActivity2) getActivity()).getUsuario();
+            tvNombres.setText(u.getNombres()+" "+u.getApellidos());
+            tvTelefono.setText(u.getTelefono());
+            tvCorreo.setText(u.getCorreo());
+            tvDireccion.setText(u.getDireccion());
+        }
+    }
+
+    private void enlazarControles(View view){
+        tvNombres = view.findViewById(R.id.tvNombreCuenta);
+        tvTelefono = view.findViewById(R.id.tvTelefonoCuenta);
+        tvCorreo = view.findViewById(R.id.tvCorreoCuenta);
+        tvDireccion = view.findViewById(R.id.tvDireccionCuenta);
     }
 }
